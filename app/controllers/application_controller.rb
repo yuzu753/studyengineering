74570e5b7d9e76616c7dfb87f8e7abce84ad1bca
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2019-12-05 12:19:10 +0900

    ルーティングと各ページへの遷移確認完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def after_sign_in_path_for(resource)
       user_path(current_user.id)
@@ -6,5 +7,11 @@ class ApplicationController < ActionController::Base
 
     def after_sign_out_path_for(resource)
       root_path
+    end
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
 end
